365e931035d7a3df154fe62de61c8749fc9171af
7-Qwen <thirdcall_7wen@163.com>
2023-07-05 18:41:03 +0800

    修改若干特性

--- pymain/FaceIdentify.py
+++ pymain/FaceIdentify.py
@@ -20,7 +20,7 @@ path = '../static/upload/faceModel'
 # 获取分类器
 faceCascade = cv2.CascadeClassifier(r'../static/data/haarcascade_frontalface_default.xml')
 # 训练集合文件
-trainerPath = '../trainer/trainer.yml'
+trainerPath = '../trainer/trainer'
 
 # flask ---
 app = Flask(__name__)
@@ -28,8 +28,8 @@ app = Flask(__name__)
 app.config['IDENTIFY_UPLOAD_FOLDER'] = IDENTIFY_UPLOAD_FOLDER
 app.config['MODEL_UPLOAD_FOLDER'] = MODEL_UPLOAD_FOLDER
 # 设置返回集合
-data_false = {"isSuccess": False}
-data_true = {"isSuccess": True}
+data_false = {"isSuccess": False, "code": 300}
+data_true = {"isSuccess": True, "code": 200}
 
 
 # 获取允许的文件类型
@@ -45,52 +45,59 @@ def Face():
     if request.method == 'POST':
         # 校验文件是否合法
         if 'file' not in request.files:
-            return jsonify(data_false, "file不存在")
+            return jsonify(result=data_false, message="file不存在")
         file = request.files['file']
         if file.filename == '':
-            return jsonify(data_false, "文件名称为空")
+            return jsonify(result=data_false, message="文件名称为空")
         # 安全校验filename
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['IDENTIFY_UPLOAD_FOLDER'], filename))
-            # 加载训练好的模型文件
-            recognizer.read(trainerPath)
-            # 图片地址
-            fiPath = '../static/upload/identifyModel/' + filename
-            # 获取图片
-            img = cv2.imread(fiPath)
-            # 图片灰度化
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            # 检测人脸
-            # 参数：
-            # scaleFactor(比例因子)：图片缩放多少，minNeighbors:至少检测多少次，
-            # minSize:当前检测区域的最小面积
-            # maxSize:当前检测区域的最面积
-            # faces = face_detector.detectMultiScale(gray)
-            # scaleFactor=1.01, minNeighbors=3, maxSize = (33, 33), minSize = (28, 28)
-            faces = faceCascade.detectMultiScale(
-                gray,
-                scaleFactor=1.3,
-                minNeighbors=5,
-                flags=cv2.CASCADE_SCALE_IMAGE
-            )
-            for (x, y, w, h) in faces:
-                # 画一个矩形
-                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                # predict()函数返回两个元素的数组：第一个元素是所识别 个体的标签， 第二个是置信度评分。
-                idnum, confidence = recognizer.predict(gray[y:y + h, x:x + w])
-                # 设置匹配指数
-                score = int("{0}".format(round(100 - confidence)))
-                # 匹配指数大于等于60即可验证通过人脸
-                print("score++++:", score)
-                if score > 60:
+            if "modelId" in request.form:
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['IDENTIFY_UPLOAD_FOLDER'], filename))
+                # 图片地址
+                fiPath = '../static/upload/identifyModel/' + filename
+                # 加载训练好的模型文件
+                trainRealPath = trainerPath + str(int(request.form.get("modelId"))) + ".yml"
+                if not os.path.exists(trainRealPath):
                     os.remove(fiPath)
-                    return jsonify(data_true)
-                else:
-                    os.remove(fiPath)
-                    return jsonify(data_false, "验证不通过")
+                    return jsonify(result=data_false, message="modelId有误")
+                recognizer.read(trainRealPath)
+                # 获取图片
+                img = cv2.imread(fiPath)
+                # 图片灰度化
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                # 检测人脸
+                # 参数：
+                # scaleFactor(比例因子)：图片缩放多少，minNeighbors:至少检测多少次，
+                # minSize:当前检测区域的最小面积
+                # maxSize:当前检测区域的最面积
+                # faces = face_detector.detectMultiScale(gray)
+                # scaleFactor=1.01, minNeighbors=3, maxSize = (33, 33), minSize = (28, 28)
+                faces = faceCascade.detectMultiScale(
+                    gray,
+                    scaleFactor=1.3,
+                    minNeighbors=5,
+                    flags=cv2.CASCADE_SCALE_IMAGE
+                )
+                for (x, y, w, h) in faces:
+                    # 画一个矩形
+                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    # predict()函数返回两个元素的数组：第一个元素是所识别 个体的标签， 第二个是置信度评分。
+                    idnum, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+                    # 设置匹配指数
+                    score = int("{0}".format(round(100 - confidence)))
+                    # 匹配指数大于等于60即可验证通过人脸
+                    print("score++++:", score)
+                    if score > 60:
+                        os.remove(fiPath)
+                        return jsonify(result=data_true, modelId=idnum)
+                    else:
+                        os.remove(fiPath)
+                        return jsonify(result=data_false, message="验证不通过")
+        else:
+            return jsonify(result=data_false, message="没有指定modelId,请先建立模型")
     else:
-        return jsonify(data_false, "请求方式不为POST")
+        return jsonify(result=data_false, message="请求方式不为POST")
 
 
 # 获取图像及标签
@@ -100,10 +107,10 @@ def getImagesAndLabels():
     if request.method == 'POST':
         # 校验文件是否合法
         if 'file' not in request.files:
-            return jsonify(data_false, "file不存在")
+            return jsonify(result=data_false, message="file不存在")
         file = request.files['file']
         if file.filename == '':
-            return jsonify(data_false, "文件名称为空")
+            return jsonify(result=data_false, message="文件名称为空")
         # 安全校验filename
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
@@ -112,22 +119,29 @@ def getImagesAndLabels():
             imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
             faceSamples = []
             ids = []
+            returnId = 0
             for imagePath in imagePaths:
                 PIL_img = Image.open(imagePath).convert('L')
                 img_numpy = np.array(PIL_img, 'uint8')
                 # 图片按照user.id.序列.jpg排列
-                id = int(os.path.split(imagePath)[-1].split(".")[1])
+                if "modelId" in request.form:
+                    # 检查传递的modelId是否为已存在的id 如果是则重复录入提高准确性如果不是则新建立人脸id
+                    id = int(request.form.get("modelId"))
+                else:
+                    id = np.random.randint(low=11111, high=99999)
+                returnId = id
                 faces = faceCascade.detectMultiScale(img_numpy)
                 for (x, y, w, h) in faces:
                     faceSamples.append(img_numpy[y:y + h, x: x + w])
                     ids.append(id)
+                os.remove(imagePath)
             recognizer.train(faceSamples, np.array(ids))
             print("Training...")
-            recognizer.write(trainerPath)
+            recognizer.save(trainerPath + str(returnId) + '.yml')
             print("Done.")
-            return jsonify(data_true)
+            return jsonify(result=data_true, modelId=returnId)
     else:
-        return jsonify(data_false, "请求方式不为POST")
+        return jsonify(result=data_false, message="请求方式不为POST")
 
 # if __name__ == '__main__':
 #     name, confidence = Face()
